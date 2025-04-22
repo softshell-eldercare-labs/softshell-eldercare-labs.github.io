311fc20752aad35390a118a3dd2d650240360337
softshell-eldercare-labs <softshell.eldercare@gmail.com>
2025-04-22 14:37:42 +0000

    Deploying to gh-pages from @ softshell-eldercare-labs/softshell-eldercare-labs.github.io@603aeddd4de1f36b60ec6afd3beb87c38fc23025 🚀

--- docs/wasm/softshell-eldercare-labs.d.ts
+++ docs/wasm/softshell-eldercare-labs.d.ts
@@ -9,16 +9,20 @@ export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembl
 
 export interface InitOutput {
   readonly memory: WebAssembly.Memory;
-  readonly __wbg_jsowner_free: (a: number, b: number) => void;
   readonly main: (a: number, b: number) => number;
-  readonly __wbindgen_export_0: (a: number, b: number) => number;
-  readonly __wbindgen_export_1: (a: number, b: number, c: number, d: number) => number;
-  readonly __wbindgen_export_2: (a: number) => void;
-  readonly __wbindgen_export_3: (a: number, b: number, c: number) => void;
+  readonly __wbg_jsowner_free: (a: number, b: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
   readonly __wbindgen_export_4: WebAssembly.Table;
-  readonly __wbindgen_export_5: (a: number, b: number, c: number) => void;
-  readonly __wbindgen_export_6: (a: number, b: number) => void;
-  readonly __wbindgen_export_7: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __externref_drop_slice: (a: number, b: number) => void;
+  readonly __wbindgen_export_7: WebAssembly.Table;
+  readonly _ZN132__LT_dyn_u20_core__ops__function__FnMut_LT__LP__RP__GT__u2b_Output_u20__u3d__u20_R_u20_as_u20_wasm_bindgen__closure__WasmClosure_GT_8describe6invoke17ha51309ad6e4dfa95E: (a: number, b: number) => void;
+  readonly closure199_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure205_externref_shim: (a: number, b: number, c: any) => void;
+  readonly closure526_externref_shim: (a: number, b: number, c: any) => void;
   readonly __wbindgen_start: () => void;
 }
 
--- docs/wasm/softshell-eldercare-labs_bg.wasm.d.ts
+++ docs/wasm/softshell-eldercare-labs_bg.wasm.d.ts
@@ -1,14 +1,18 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export const __wbg_jsowner_free: (a: number, b: number) => void;
 export const main: (a: number, b: number) => number;
-export const __wbindgen_export_0: (a: number, b: number) => number;
-export const __wbindgen_export_1: (a: number, b: number, c: number, d: number) => number;
-export const __wbindgen_export_2: (a: number) => void;
-export const __wbindgen_export_3: (a: number, b: number, c: number) => void;
+export const __wbg_jsowner_free: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
 export const __wbindgen_export_4: WebAssembly.Table;
-export const __wbindgen_export_5: (a: number, b: number, c: number) => void;
-export const __wbindgen_export_6: (a: number, b: number) => void;
-export const __wbindgen_export_7: (a: number, b: number, c: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __externref_drop_slice: (a: number, b: number) => void;
+export const __wbindgen_export_7: WebAssembly.Table;
+export const _ZN132__LT_dyn_u20_core__ops__function__FnMut_LT__LP__RP__GT__u2b_Output_u20__u3d__u20_R_u20_as_u20_wasm_bindgen__closure__WasmClosure_GT_8describe6invoke17ha51309ad6e4dfa95E: (a: number, b: number) => void;
+export const closure199_externref_shim: (a: number, b: number, c: any) => void;
+export const closure205_externref_shim: (a: number, b: number, c: any) => void;
+export const closure526_externref_shim: (a: number, b: number, c: any) => void;
 export const __wbindgen_start: () => void;
